42a9be8563735d6eeb3b7fee1e6d17ae578d8bc8
Leonid Levchishin <superdjskater@mail.ru>
2024-03-03 23:08:52 +0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+mod inet;
+mod fs;
+mod user;
+
+fn main() -> Result<(), eframe::Error>
+{
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1000f32, 600f32]),
+        ..Default::default()
+    };
+
+     eframe::run_native(
+        "Seira",
+        options,
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Box::<app::Seira>::default()
+        }),
+    )
 }
